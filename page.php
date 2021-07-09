0a17b92b25a6c48847568407ef3b6eb7539677bf
Brian Cobaleda <yosoy@briancobaleda.com>
2021-07-09 16:13:03 -0500

    se diseño la zona lateral de page.php

--- page.php
+++ page.php
@@ -1,19 +1,62 @@
 <?php get_header(); ?>
-<div class="contenedor mt-1 sombra-lg rounded p-5 border-box">
-<?php
-        // Pregunta si hay post (devuelve true o false)
-        if(have_posts()){
-            // si hay post los itera en un ciclo
-            while(have_posts()){
-                // almacena el post en la funcion the_post()
-                the_post() ;?>
-                    <!-- imprimer el titulo -->
-                    <h1><?php the_title()?></h1>
-                    <!-- imprime el contenido -->
-                    <?php the_content()?>
-                <?php
+<?php $url = get_template_directory_uri(  ); ?>
+<div class="page_container contenedor rounded mt-2 border-box">
+    <div class="contenido">
+    <?php
+                // Pregunta si hay post (devuelve true o false)
+                if(have_posts()){
+                    // si hay post los itera en un ciclo
+                    while(have_posts()){
+                        // almacena el post en la funcion the_post()
+                        the_post() ;?>
+                            <!-- imprimer el titulo -->
+                            <h1><?php the_title()?></h1>
+                            <!-- imprime el contenido -->
+                            <?php the_content()?>
+                        <?php
+                    }
+                }
+            ?>
+    </div>
+    <div class="lateral">
+        <h2>¿ya Conoces nuestros productos destacados?</h2>
+        <p>Nuestros productos son únicos en sabor, qué esperas a conocerlos</p>
+        <div class="producto-destacado">
+            <?php
+                // $numero = rand(1,4); 
+                $arg = array(
+                    // nombre del post type creado en function.php
+                    'post_type'             => 'producto',
+                    'posts_per_page'         => 2,
+                    // 'order'                 => 'ASC',
+                    // 'orderby'               => 'date,'
+                    'orderby'               => 'rand'
+                );
+            
+            $productos = new WP_Query($arg);
+            
+            if($productos->have_posts( )){
+                while($productos->have_posts()){
+                    $productos->the_post(  );
+
+                    ?>
+                    
+                        <a class="text-dark" href="<?php the_permalink( ) ?>">
+                        
+                        <div class="">
+                            <?php the_post_thumbnail('thumbnail'); ?>
+                        </div>
+                        <div class="py-1 mx-2 texto-bold  texto-naranja texto-centrado">
+                        <?php the_title(  ) ?>
+                        </div>
+                    </a> 
+                    
+                    <?php
+                }
             }
-        }
-    ?>
+            ?>
+        </div>
+        hola
+    </div>
 </div>
 <?php get_footer(); ?>
