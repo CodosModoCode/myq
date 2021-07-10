6d2de27c8ef2904662aca26b51b7db3570adaf30
Brian Cobaleda <yosoy@briancobaleda.com>
2021-07-10 14:31:10 -0500

    bebe, ya le hices al header,, le puse los botones dinámicos

--- category-sedes.php
+++ category-sedes.php
@@ -6,21 +6,23 @@
                 while (have_posts(  )) {
                     the_post(  )
                     ?>
-                    <a href="<?php the_permalink() ?>">
+                    
                         <div class="post-item">
-                            <div>
+                            <div class="imagen">
                                 <?php the_post_thumbnail('medium') ?>
                             </div>
-                            <div class="p-1">
-                                <h2><?php the_title(  )?></h2> 
-                                <div class="texto-negro-light">
-                                    <div class=""><?php the_field( 'telefono_movil' ); ?></div>
+                            <div class="d-flex-column-left">
+                                <div class="texto-negro p-2">
+                                    <h2><?php the_title(  )?></h2> 
+                                    <div class=""><span class="texto-bold">Teléfono:</span> <?php the_field( 'telefono_movil' ); ?></div>
                                     <div class=""><span class="texto-bold">Dirección:</span> <?php the_field( 'direccion' ); ?></div>
                                     <div class=""><span class="texto-bold">Horario de Atención:</span> <?php the_field( 'horario_de_atencion' ); ?></div>
+                                    <hr class="texto-gris my-1" style="opacity:.2">
+                                    <a href="<?php the_permalink() ?>"><button class="btn-magenta">Ver Sede</button></a>
                                 </div>
                             </div>
                         </div>
-                    </a>
+                    
                     <?php
                 }
             }
